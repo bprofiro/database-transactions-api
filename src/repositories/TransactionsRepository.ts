c8c06211ab3a4535ef71203fb41359726e3a1366
Brenda Profiro <brendaprofiro@gmail.com>
2020-04-18 15:13:38 -0300

    done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,6 +1,7 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, getRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+// import AppError from '../errors/AppError';
 
 interface Balance {
   income: number;
@@ -11,7 +12,33 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = getRepository(Transaction);
+
+    const checkBlance = await transactions.find();
+
+    const income = checkBlance
+      .filter(transaction => {
+        return transaction.type === 'income';
+      })
+      .reduce((total, transaction) => {
+        return total + transaction.value;
+      }, 0);
+
+    const outcome = checkBlance
+      .filter(transaction => {
+        return transaction.type === 'outcome';
+      })
+      .reduce((total, transaction) => {
+        return total + transaction.value;
+      }, 0);
+
+    const total = income - outcome;
+
+    // if (outcome > total) {
+    //   throw new AppError('You need mor money fot this!');
+    // }
+
+    return { income, outcome, total };
   }
 }
 
